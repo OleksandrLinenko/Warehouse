ce75b61928a6d8a2a0cf779fb86337e1decbca69
oleksandrlinenko <oleksandrlinenko@localhost>
2025-08-08 21:42:51 +0200

    Add ListAvailabilityProductsByFiltetCommand, LoadDataCommand, SaveDataCommand and SendProductCommand classes

---
+++ src/Commands/ListAvailbProductsByFilters.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Commands;
+
+/**
+ *
+ * @author oleksandrlinenko
+ */
+public class ListAvailbProductsByFilters {
+
+    public static ListAvailbProductsByFilters create() {
+        return new ListAvailbProductsByFilters();
+    }
+
+    public void handle() {
+
+    }
+}
