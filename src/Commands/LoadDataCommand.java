439924ece8d530cdf22cca2e1701cf602faa2b20
oleksandrlinenko <oleksandrlinenko@localhost>
2025-08-09 19:09:51 +0200

    Refactor LoadDataCommand

--- src/Commands/LoadDataCommand.java
+++ src/Commands/LoadDataCommand.java
@@ -4,17 +4,27 @@
  */
 package Commands;
 
+import UI.Ask;
+import UI.ErrorMessage;
+import Warehouse.Data;
+import java.io.IOException;
+
 /**
  *
  * @author oleksandrlinenko
  */
 public class LoadDataCommand {
-
+    
     public static LoadDataCommand create() {
         return new LoadDataCommand();
     }
-
+    
     public void handle() {
-
+        try {
+            String path = Ask.create().askString("Set path to file: ");
+            Data.create().loadData(path);
+        } catch (IOException ex) {
+            ErrorMessage.create().show("Error while loading data");
+        }
     }
 }
