913bdfdeb61e74bcf33099e044af8149063019bd
oleksandrlinenko <oleksandrlinenko@localhost>
2025-08-09 19:06:38 +0200

    Add Ask class

---
+++ src/UI/Ask.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author oleksandrlinenko
+ */
+public class Ask {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static Ask create() {
+        return new Ask();
+    }
+
+    public String askString(String string) {
+        Message.create().show(string);
+        return sc.next();
+    }
+}
