e9496bbe63125b98e4671eb08ae91bd66ba7883b
oleksandrlinenko <oleksandrlinenko@localhost>
2025-08-10 19:31:46 +0200

    Refactor SendProductCommand class

--- src/Commands/SendProductCommand.java
+++ src/Commands/SendProductCommand.java
@@ -4,17 +4,33 @@
  */
 package Commands;
 
+import UI.Ask;
+import UI.Message;
+import Warehouse.Application;
+import Warehouse.Product;
+
 /**
  *
  * @author oleksandrlinenko
  */
 public class SendProductCommand {
-
+    
     public static SendProductCommand create() {
         return new SendProductCommand();
     }
-
+    
     public void handle() {
-
+        String name = Ask.create().askString("Set product name you need send to customer: ");
+        int quantity = Ask.create().askInt("Set product quantity you need send to customer: ");
+        for (Product product : Application.getInstance().getProducts().getProducts()) {
+            if (product.getName().equals(name) && product.getQuantity() >= quantity) {
+                Message.create().show("Customer products have been succesfully sended");
+                product.setQuantity(quantity);
+                product.setAvailability();
+            } else {
+                Message.create().show("Currently where are not enough quantity of selected product or it's soldout");
+            }
+        }
+        
     }
 }
